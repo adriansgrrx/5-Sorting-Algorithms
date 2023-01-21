10a851b29ce39208bd3f445f6de88488038bc709
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2023-01-21 22:31:05 +0800

    5 steps for selection sort to be executed, implemented.

---
+++ selection_sort.py
@@ -0,0 +1,5 @@
+# 1. Create an unsorted array
+# 2. Create a function named selection_sort
+# 3. Create an iteration that will iterate with the array's length
+# 4. Compare the left-side element to the whole array to find the smallest value
+# 5. If found, swap the two values. Repeat.
