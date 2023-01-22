070239757ea463792904cd97f81afc411e91a266
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2023-01-22 23:08:50 +0800

    Pseudocode updated.

--- merge_sort.py
+++ merge_sort.py
@@ -2,3 +2,5 @@
 # 2. Create a function named merge_sort
 # 3. Divide the array until only one single element is left
 # 4. sort the groups recursively.
+# 5. return the array
+# 6. call the function
