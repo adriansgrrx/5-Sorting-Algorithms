5e1b238ee46853839e2fe1236e9b100881f73b63
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2023-01-22 22:09:41 +0800

    Pseudocode implemented.

---
+++ insertion_sort.py
@@ -0,0 +1,6 @@
+# 1. Create an unsorted array
+# 2. Create a function named insertion_sort
+# 3. Create a for loop that will iterate the array with respect to its length
+# 4. Create a while loop that will compare a consecutive pair in the array
+# 5. return the array
+# 6. call the function
