27fb282ce67d49fb21753e3fee598740b62b8d3f
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2023-01-23 00:34:24 +0800

    Pseudocode implemented.

---
+++ quick_sort.py
@@ -0,0 +1,6 @@
+# 1. Create an unsorted array
+# 2. Create a function named merge_sort
+# 3. Select a pivot
+# 4. distribute the elements by left(less than) and right (greater than than pivot)
+# 5. return sorted sort the array
+# 6. call the function
