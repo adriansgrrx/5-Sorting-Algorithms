033075fd9aa36ea67e9e465dfd5a8df7f3be714b
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2023-01-22 21:02:38 +0800

    Pseudocode implemented.

---
+++ bubble_sort.py
@@ -0,0 +1,7 @@
+# 1. Create an unsorted array
+# 2. Create a function named bubble_sort
+# 3. Get the length of the array
+# 4. Traverse the array from 0 to n-i-1
+    # Swap if the element found is greater
+    # than the next element
+# 5. Call the function
