254bf2d38716010a79d03543825a7ebbeb918a80
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2023-01-23 19:32:55 +0800

    Changed some comments.

--- quick_sort.py
+++ quick_sort.py
@@ -1,13 +1,13 @@
 # 1. Create an unsorted array
 myNums = [20, 83, 99, 28, 37, 45, 74, 16, 9, 35]
-# 2. Create a function named merge_sort
+# 2. Create a function named quick_sort
 def quick_sort(arr):
 # 3. Select a pivot
     # to select a pivot, we need to divide the array
     if len(arr) <= 1:
         return arr
     pivot = arr[len(arr)//2]
-# 4. distribute the elements by left(less than) and right (greater than than pivot)
+# 4. distribute the elements to left(if less than the pivot) and to right ( if greater than the pivot)
     left = []
     for element in arr:
         if element < pivot:
@@ -22,7 +22,7 @@ def quick_sort(arr):
     for element in arr:
         if element > pivot:
             right.append(element)
-# 5. return sorted sort the array
+# 5. return the sorted array
     sorted_array = quick_sort(left) + middle + quick_sort(right)
     return sorted_array
 # 6. call the function
